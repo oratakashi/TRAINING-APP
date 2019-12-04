db27cf5dd8ea02e7a51098f7fffac09c8d628d38
Oratakashi Nhamako <davidcreazy@gmail.com>
2019-12-04 17:19:24 +0700

    add API Class

---
+++ app/src/main/java/id/oratakashi/training/data/network/Api.java
@@ -0,0 +1,37 @@
+package id.oratakashi.training.data.network;
+
+import java.util.concurrent.TimeUnit;
+
+import id.oratakashi.training.BuildConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Api {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getUrl(){
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.readTimeout(10, TimeUnit.SECONDS);
+        builder.connectTimeout(30, TimeUnit.SECONDS);
+
+        if(BuildConfig.DEBUG){
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(interceptor);
+        }
+
+        OkHttpClient client = builder.build();
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl( BuildConfig.BASE_URL )
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
